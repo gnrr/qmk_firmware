aa2c404819a403e3a914670cf5226a54b93f973d
hidsh <hideaki.sh@gmail.com>
2018-10-28 14:35:22 +0900

    first make done asceension

--- keyboards/handwired/asceension/keymaps/hidsh/readme.md
+++ keyboards/handwired/asceension/keymaps/hidsh/readme.md
@@ -3,6 +3,6 @@ Default Keymap
 
 Super simple default keymap.
 
-Keymap Maintainer: [amnobis](https://github.com/amnobis)
+Keymap Maintainer: [hidsh](https://github.com/hidsh)
 
 Intended usage: This is mostly provided for testing before you build your own keymap and as a reference to a stock(ish) configuration
--- keyboards/handwired/asceension/matrix.c
+++ keyboards/handwired/asceension/matrix.c
@@ -60,14 +60,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     extern const matrix_row_t matrix_mask[];
 #endif
 
-static const uint8_t col_pins[ATMEGA_COLS] = MATRIX_COL_PINS;
 static const uint8_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 
 /* matrix state(1:on, 0:off) */
 static matrix_row_t matrix[MATRIX_ROWS];
 static matrix_row_t matrix_debouncing[MATRIX_ROWS];
 static uint8_t mcp23018_reset_loop;
-
 
 static void init_cols(void);
 static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row);
@@ -115,16 +113,16 @@ uint8_t matrix_cols(void) {
 void matrix_init(void) {
 
     /* To use PORTF disable JTAG with writing JTD bit twice within four cycles. */
-    #if  (defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__) || defined(__AVR_ATmega32U4__))
-        MCUCR |= _BV(JTD);
-        MCUCR |= _BV(JTD);
-    #endif
+#if  (defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__) || defined(__AVR_ATmega32U4__))
+    MCUCR |= _BV(JTD);
+    MCUCR |= _BV(JTD);
+#endif
 
     mcp23018_status = true;
 
     /* initialize row and col */
     unselect_rows();
-    init_cols();
+    init_cols();                                            // all cols is scan via MCP23018
 
     /* initialize matrix state: all keys off */
     for (uint8_t i=0; i < MATRIX_ROWS; i++) {
@@ -137,6 +135,7 @@ void matrix_init(void) {
 
 uint8_t matrix_scan(void)
 {
+    // init MCP23018
     if (mcp23018_status) { 
         /* if there was an error */
         if (++mcp23018_reset_loop == 0) {
@@ -144,36 +143,37 @@ uint8_t matrix_scan(void)
                this will be approx bit more frequent than once per second */
             print("trying to reset mcp23018\n");
             mcp23018_status = init_mcp23018();
+
             if (mcp23018_status) {
-                print("left side not responding\n");
+                print("MCP23018 not responding\n");
             } else {
-                print("left side attached\n");
+                print("MCP23018 attached\n");
             }
         }
     }
 
     /* Set row, read cols */
     for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
-#       if (DEBOUNCING_DELAY > 0)
-            bool matrix_changed = read_cols_on_row(matrix_debouncing, current_row);
+#if (DEBOUNCING_DELAY > 0)
+        bool matrix_changed = read_cols_on_row(matrix_debouncing, current_row);
 
-            if (matrix_changed) {
-                debouncing = true;
-                debouncing_time = timer_read();
-            }
-#       else
-            read_cols_on_row(matrix, current_row);
-#       endif
+        if (matrix_changed) {
+            debouncing = true;
+            debouncing_time = timer_read();
+        }
+#else
+        read_cols_on_row(matrix, current_row);
+#endif
     }
 
-#   if (DEBOUNCING_DELAY > 0)
-        if (debouncing && (timer_elapsed(debouncing_time) > DEBOUNCING_DELAY)) {
-            for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-                matrix[i] = matrix_debouncing[i];
-            }
-            debouncing = false;
+#if (DEBOUNCING_DELAY > 0)
+    if (debouncing && (timer_elapsed(debouncing_time) > DEBOUNCING_DELAY)) {
+        for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+            matrix[i] = matrix_debouncing[i];
         }
-#   endif
+        debouncing = false;
+    }
+#endif
 
     matrix_scan_quantum();
     return 1;
@@ -190,7 +190,7 @@ bool matrix_is_modified(void)
 inline
 bool matrix_is_on(uint8_t row, uint8_t col)
 {
-    return (matrix[row] & ((matrix_row_t)1<col));
+    return (matrix[row] & ((matrix_row_t)1 << col));
 }
 
 inline
@@ -227,15 +227,21 @@ uint8_t matrix_key_count(void)
 
 static void init_cols(void)
 {
+#if 1 // all cols uses MCP23018
+    // do nothing because MCP23018 is initialized every cycles 
+#else
     for(uint8_t x = 0; x < ATMEGA_COLS; x++) {
         uint8_t pin = col_pins[x];
         _SFR_IO8((pin >> 4) + 1) &= ~_BV(pin & 0xF); /* IN */
         _SFR_IO8((pin >> 4) + 2) |=  _BV(pin & 0xF); /* HI */
     }
+#endif
 }
 
 static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
 {
+    cols_t cols;
+
     /* Store last value of row prior to reading */
     matrix_row_t last_row_value = current_matrix[current_row];
 
@@ -246,31 +252,19 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
     select_row(current_row);
     wait_us(30);
 
-    if (mcp23018_status) { 
-        /* if there was an error */
-        return 0;
-    } else {
-        uint16_t data = 0;
-        mcp23018_status = i2c_start(I2C_ADDR_WRITE);    if (mcp23018_status) goto out;
-        mcp23018_status = i2c_write(GPIOA);             if (mcp23018_status) goto out;
-        mcp23018_status = i2c_start(I2C_ADDR_READ);     if (mcp23018_status) goto out;
-        data = i2c_readNak();
-        data = ~data;
-    out:
-        i2c_stop();
-        current_matrix[current_row] |= (data << 8);
-    }
+    /* read all cols from MCP23018 */
+    mcp23018_status = i2c_start(I2C_ADDR, I2C_WRITE);    if(mcp23018_status) goto out;
+    mcp23018_status = i2c_write(GPIOA);                  if(mcp23018_status) goto out;
+    mcp23018_status = i2c_rep_start(I2C_ADDR, I2C_READ); if(mcp23018_status) goto out;
+    mcp23018_status = i2c_read_ack(&cols.byte.lo);       if(mcp23018_status) goto out;
+    /* Now read to IODIRB */
+    mcp23018_status = i2c_read_nak(&cols.byte.hi);       if(mcp23018_status) goto out;
 
-    /* For each col... */
-    for(uint8_t col_index = 0; col_index < ATMEGA_COLS; col_index++) {
-        /* Select the col pin to read (active low) */
-        uint8_t pin = col_pins[col_index];
-        uint8_t pin_state = (_SFR_IO8(pin >> 4) & _BV(pin & 0xF));
+out:
+    i2c_stop();
 
-        /* Populate the matrix row with the state of the col pin */
-        current_matrix[current_row] |=  pin_state ? 0 : (ROW_SHIFTER << col_index);
-    }
-
+    current_matrix[current_row] = cols.word;
+    
     /* Unselect row */
     unselect_rows();
 
@@ -279,6 +273,7 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
 
 static void select_row(uint8_t row)
 {
+#if 0 // all rows use ATMEGA32u4 GPIOs
     if (mcp23018_status) { 
         /* if there was an error do nothing */
     } else {
@@ -291,9 +286,10 @@ static void select_row(uint8_t row)
     out:
         i2c_stop();
     }
-
+#endif
+    
     uint8_t pin = row_pins[row];
-    _SFR_IO8((pin >> 4) + 1) |=  _BV(pin & 0xF); /*  OUT  */
+    _SFR_IO8((pin >> 4) + 1) |=  _BV(pin & 0xF); /* OUT  */
     _SFR_IO8((pin >> 4) + 2) &= ~_BV(pin & 0xF); /* LOW  */
 }
 
@@ -301,7 +297,7 @@ static void unselect_rows(void)
 {
     for(uint8_t x = 0; x < MATRIX_ROWS; x++) {
         uint8_t pin = row_pins[x];
-        _SFR_IO8((pin >> 4) + 1) &= ~_BV(pin & 0xF); /* IN */
-        _SFR_IO8((pin >> 4) + 2) |=  _BV(pin & 0xF); /* HI */
+        _SFR_IO8((pin >> 4) + 1) |=  _BV(pin & 0xF); /* OUT */
+        _SFR_IO8((pin >> 4) + 2) |=  _BV(pin & 0xF); /* HI  */
     }
 }
