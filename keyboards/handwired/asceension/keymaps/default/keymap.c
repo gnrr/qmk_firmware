a058550976c091c4e30059ca93eb8dcf70d78b64
hidsh <hideaki.sh@gmail.com>
2018-11-12 05:26:32 +0900

    mod

--- keyboards/handwired/asceension/keymaps/default/keymap.c
+++ keyboards/handwired/asceension/keymaps/default/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             {KC_F18,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,   KC_NO},
             {KC_LSFT,  KC_HOME,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH,  KC_PGDN,  KC_PGUP},
             {KC_NO,    KC_NO,    KC_NO,    KC_FN0,   KC_LGUI,  KC_BTN2,  KC_ESC,   KC_SPC,   KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT,  KC_NO,    KC_NO},
-            {KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_LALT,  KC_LCTRL, KC_LSFT,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO} },
+            {KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_LGUI,  KC_LCTRL, KC_LSFT,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO} },
 
 	[1] = { {KC_NO,    RESET,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS},
             {KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS},
