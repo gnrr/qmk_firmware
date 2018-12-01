dc4cecf49f5e536ce0c4d2ed55b744e948b013ab
hidsh <hideaki.sh@gmail.com>
2018-12-01 15:22:54 +0900

    mod

--- keyboards/handwired/asceension/dipsw.c
+++ keyboards/handwired/asceension/dipsw.c
@@ -5,19 +5,19 @@
 void read_dipsw(dsw_t* pdsw)
 {
     //        ON    OFF      action
-    // DSW0 
     // DSW1 
     // DSW2 
-    // DSW3 : Win   Mac     change keymap
+    // DSW3 
+    // DSW4 : Win   Mac     change keymap
 
     // lo-active
-    pdsw->dsw0_opt  = ((PIND & (1 << 4)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW0 PD4: ON(lo)=ON, OFF(hi)=Win
-    pdsw->dsw1_opt  = ((PIND & (1 << 6)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW1 PD6: ON(lo)=ON,  OFF(hi)=OFF
-    pdsw->dsw2_opt  = ((PIND & (1 << 7)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW2 PD7: ON(lo)=ON,  OFF(hi)=OFF
-    pdsw->dsw3_host = ((PINE & (1 << 6)) == 0)? DSW_KEYMAP_WIN:DSW_KEYMAP_MAC; // DSW3 PE6: ON(lo)=Win,  OFF(hi)=Mac
+    pdsw->dsw1_opt  = ((PIND & (1 << 4)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW1 PD4: ON(lo)=ON,   OFF(hi)=OFF
+    pdsw->dsw2_opt  = ((PIND & (1 << 6)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW2 PD6: ON(lo)=ON,   OFF(hi)=OFF
+    pdsw->dsw3_opt  = ((PIND & (1 << 7)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW3 PD7: ON(lo)=ON,   OFF(hi)=OFF
+    pdsw->dsw4_host = ((PINE & (1 << 6)) == 0)? DSW_KEYMAP_WIN:DSW_KEYMAP_MAC; // DSW4 PE6: ON(lo)=Win,  OFF(hi)=Mac
 
     uint8_t default_layer;
-    default_layer = (1<<(pdsw->dsw3_host));
+    default_layer = (1<<(pdsw->dsw4_host));
     default_layer_set((uint32_t)default_layer);
 }
 
--- keyboards/handwired/asceension/dipsw.h
+++ keyboards/handwired/asceension/dipsw.h
@@ -4,10 +4,11 @@ typedef enum {DSW_KEYMAP_MAC=0, DSW_KEYMAP_WIN} dsw_host_t;
 typedef enum {DSW_OPT_OFF=0, DSW_OPT_ON} dsw_opt_t;
 
 typedef struct {
-    dsw_opt_t dsw0_opt;
     dsw_opt_t dsw1_opt;
     dsw_opt_t dsw2_opt;
-    dsw_opt_t dsw3_host;
+    dsw_opt_t dsw3_opt;
+    dsw_opt_t dsw4_host;
 } dsw_t;
 
 extern void read_dipsw(dsw_t* pdsw);
+
