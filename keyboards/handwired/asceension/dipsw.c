902e4e861fad3ca392f5a939064c260b2b21dc11
hidsh <hideaki.sh@gmail.com>
2018-12-01 15:19:07 +0900

    mod

--- keyboards/handwired/asceension/dipsw.c
+++ keyboards/handwired/asceension/dipsw.c
@@ -4,17 +4,17 @@
 
 void read_dipsw(dsw_t* pdsw)
 {
-    //        OFF   ON      action
+    //        ON    OFF      action
     // DSW0 
     // DSW1 
     // DSW2 
-    // DSW3 : MAC   WIN     change keymap
+    // DSW3 : Win   Mac     change keymap
 
     // lo-active
-    pdsw->dsw0_opt  = ((PIND & (1 << 4)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW0 PD4: ON(lo)=Mac, OFF(hi)=Win
+    pdsw->dsw0_opt  = ((PIND & (1 << 4)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW0 PD4: ON(lo)=ON, OFF(hi)=Win
     pdsw->dsw1_opt  = ((PIND & (1 << 6)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW1 PD6: ON(lo)=ON,  OFF(hi)=OFF
     pdsw->dsw2_opt  = ((PIND & (1 << 7)) == 0)? DSW_OPT_ON    :DSW_OPT_OFF;    // DSW2 PD7: ON(lo)=ON,  OFF(hi)=OFF
-    pdsw->dsw3_host = ((PINE & (1 << 6)) == 0)? DSW_KEYMAP_WIN:DSW_KEYMAP_MAC; // DSW3 PE6: ON(lo)=ON,  OFF(hi)=OFF
+    pdsw->dsw3_host = ((PINE & (1 << 6)) == 0)? DSW_KEYMAP_WIN:DSW_KEYMAP_MAC; // DSW3 PE6: ON(lo)=Win,  OFF(hi)=Mac
 
     uint8_t default_layer;
     default_layer = (1<<(pdsw->dsw3_host));
