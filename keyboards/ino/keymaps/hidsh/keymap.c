58c87ba732182ae06a6d2aa8789d8d003412dfe9
hidsh <hideaki.sh@gmail.com>
2020-07-14 07:10:42 +0900

    mod

--- keyboards/ino/keymaps/hidsh/keymap.c
+++ keyboards/ino/keymaps/hidsh/keymap.c
@@ -39,6 +39,8 @@ enum user_macro {
 #define L_SPC  MACROTAP(UM_L_SPC)   // Lower/Space
 #define R_SPC  MACROTAP(UM_R_SPC)   // Raise/Space
 
+#define US_EISU  LSFT(KC_CAPS)      // EISU on US layout
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     switch(id) {
@@ -84,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LY_MAC] = LAYOUT(
   KC_GRV,   KC_1,    KC_2,    KC_3,  KC_4,    KC_5,   KC_6,  xxx,    xxx,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINUS, KC_EQUAL, KC_BSPC, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,   xxx,   xxx,    KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS, \
-  KC_CAPS,  KC_A,    KC_S,    KC_D,  KC_F,    KC_G,   xxx,   xxx,    KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,   xxx,     \
+  US_EISU,  KC_A,    KC_S,    KC_D,  KC_F,    KC_G,   xxx,   xxx,    KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,   xxx,     \
   xxx,     KC_LSFT,  KC_Z,    KC_X,  KC_C,    KC_V,   KC_B,  xxx,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_DEL,  \
   KC_ESC,  KC_LALT,  ____, KC_LWIN, KC_SPC, KC_MSM, KC_MSL,  xxx,    KC_MSR, KC_SPC, KC_RCTL, RAISE,   LOWER,   KC_LEFT,  KC_DOWN,  KC_RIGHT \
   ),
